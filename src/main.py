d9d9cb75d66312858436ff74e438a32d02fd46f1
kevin russel <kzone159@gmail.com>
2025-11-02 23:04:22 -0600

    fix: the config file paths

--- src/main.py
+++ src/main.py
@@ -3,11 +3,16 @@
 from typing import Optional
 from pathlib import Path
 import mediapipe as mp
+import json
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 
 def model_path_exists():
+    '''
+    This function ensures that the path to the config file exists.
+    The config file holds the path to the model.
+    '''
     base_path = Path.cwd()
     model_path = Path.joinpath(base_path,"config.json")
 
@@ -17,12 +22,21 @@ def model_path_exists():
         return None
 
 def get_model_path() -> Optional[str]:
+    '''
+    Get's the model path from the config file
+    Return:
+        None -> If the config file doesn't exist
+        model_path -> if the config file exists, it gets the path of the model
+    '''
     model_path = model_path_exists()
     if not model_path:
         return None
     else:
-        return model_path
-
+        print(model_path)
+        with open(model_path, 'r') as file:
+            model_file = json.load(file)
+        model = model_file["Path_To_Model"]
+        return model
 
 
 
