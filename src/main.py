8128a3596c727c341db12c2319941e01d0d9eda5
kevin russel <kzone159@gmail.com>
2025-11-02 22:43:06 -0600

    first commit

---
+++ src/main.py
@@ -0,0 +1,18 @@
+
+
+import mediapipe as mp
+from mediapipe.tasks import python
+from mediapipe.tasks.python import vision
+
+
+
+
+
+def get_model_path() -> int:
+
+
+
+
+
+if __name__ == "__main__":
+    model_path = get_model_path()
