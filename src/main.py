61f13a9cc6414921da82c11dce011a74adb09e30
kevin russel <kzone159@gmail.com>
2025-11-02 22:52:19 -0600

    added the pathing and config.json

--- src/main.py
+++ src/main.py
@@ -1,14 +1,28 @@
 
 
+from typing import Optional
+from pathlib import Path
 import mediapipe as mp
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
 
 
+def model_path_exists():
+    base_path = Path.cwd()
+    model_path = Path.joinpath(base_path,"config.json")
 
+    if model_path.exists():
+        return model_path
+    else:
+        return None
 
+def get_model_path() -> Optional[str]:
+    model_path = model_path_exists()
+    if not model_path:
+        return None
+    else:
+        return model_path
 
-def get_model_path() -> int:
 
 
 
@@ -16,3 +30,4 @@ def get_model_path() -> int:
 
 if __name__ == "__main__":
     model_path = get_model_path()
+    print(model_path)
