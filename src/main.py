3bf6392d427edd29b4464a9ee9373f310a82b179
kevin russel <kzone159@gmail.com>
2025-11-27 21:45:44 -0600

    getting the overlay working

--- src/main.py
+++ src/main.py
@@ -6,7 +6,21 @@ import mediapipe as mp
 import json
 from mediapipe.tasks import python
 from mediapipe.tasks.python import vision
+import cv2
+import time
+latest_annotated_frame = None
 
+
+BaseOptions = mp.tasks.BaseOptions
+GestureRecognizer = mp.tasks.vision.GestureRecognizer
+GestureRecognizerOptions = mp.tasks.vision.GestureRecognizerOptions
+GestureRecognizerResult = mp.tasks.vision.GestureRecognizerResult
+VisionRunningMode = mp.tasks.vision.RunningMode
+
+
+
+def get_current_milli_time():
+    return round(time.time() * 1000)
 
 def model_path_exists() -> Optional[str]:
     '''
@@ -38,8 +52,45 @@ def get_model_path() -> Optional[str]:
 
 
 
+def print_result(result: GestureRecognizerResult, output_image: mp.Image, timestamp_ms: int):
+    
+    global latest_annotated_frame
+    print('gesture recognition result: {}'.format(result))
+    # Convert mp.Image (RGB) → numpy array
+    
+    if output_image is None:
+        return
+    
+    annotated_image = output_image.numpy_view().copy()
+    latest_annotated_frame = cv2.cvtColor(annotated_image, cv2.COLOR_RGB2BGR)
+    
+
+
+def main(model_path):
+    
+    options = GestureRecognizerOptions(
+    base_options=BaseOptions(model_asset_path=model_path),
+    running_mode=VisionRunningMode.LIVE_STREAM,
+    result_callback=print_result)
+    with GestureRecognizer.create_from_options(options) as recognizer:   
+        cam = cv2.VideoCapture(0)
+        while True:
+            check,frame = cam.read()
+            rgb_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_frame)
+            frame_timestamp_ms = get_current_milli_time()
+            recognizer.recognize_async(mp_image, frame_timestamp_ms)
+
+            key = cv2.waitKey(1)
+            if key == 27:
+                break
+
+        cam.release()
+        cv2.destroyAllWindows()
+        
 
 
 if __name__ == "__main__":
     model_path = get_model_path()
+    main(model_path=model_path)
     print(model_path)
